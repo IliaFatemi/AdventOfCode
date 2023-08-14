9edb0f524790e9fef075549b081f0d974106af84
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-14 19:59:14 -0400

    implimented roundverdict to return a score

---
+++ day2/day2.py
@@ -0,0 +1,42 @@
+'''
+opponent:
+
+A --> Rock
+B --> Paper
+C --> Scissor
+
+user: 
+
+X --> Rock
+Y --> Paper
+Z --> Scissor
+
+Round Verdict:
+Win --> 6
+Draw --> 3
+Loose --> 0
+'''
+
+def getPuzzleInput():
+    with open('adventofcode.com_2022_day_2_input.txt', 'r') as file:
+        game_Rounds = []
+        for i in file.readlines():
+            round_outputs = i.strip('\n').split(' ')
+            game_Rounds.append((round_outputs[0], round_outputs[1]))
+    return game_Rounds
+    
+    
+def RoundVerdict(opponent, user):
+    if (user == 'X' and opponent == 'A') or (user == 'Y' and opponent == 'B') or (user == 'Z' and opponent == 'C'):
+        return 3 
+    elif (user == 'X' and opponent == 'C') or (user == 'Y' and opponent == 'A') or (user == 'Z' and opponent == 'B'):
+        return 6
+    else:
+        return 0
+    
+
+
+    
+
+# if __name__ == "__main__":
+#     # getPuzzleInput()
