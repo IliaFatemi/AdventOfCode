45f5e35c718ade39cb4140ebcfc89e6325933ade
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-15 11:08:44 -0400

    Added unit tests to setToLoose function

--- day2/day2test.py
+++ day2/day2test.py
@@ -1,4 +1,4 @@
-from day2 import RoundVerdict, userScoreRule
+from day2 import RoundVerdict, userScoreRule, setToLoose
 
 assert RoundVerdict('A', 'Y') == 6
 assert RoundVerdict('B', 'X') == 0
@@ -7,3 +7,7 @@ assert RoundVerdict('C', 'Z') == 3
 assert userScoreRule('X') == 1
 assert userScoreRule('Y') == 2
 assert userScoreRule('Z') == 3
+
+assert setToLoose('A') == 'Z'
+assert setToLoose('B') == 'X'
+assert setToLoose('C') == 'Y'
