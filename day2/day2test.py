684066552b23fd225fb3846e2d348bfd33911894
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-15 11:42:13 -0400

    Added unit tests for setToWin function

--- day2/day2test.py
+++ day2/day2test.py
@@ -1,4 +1,4 @@
-from day2 import RoundVerdict, userScoreRule, setToLoose
+from day2 import RoundVerdict, userScoreRule, setToLoose, setToWin
 
 assert RoundVerdict('A', 'Y') == 6
 assert RoundVerdict('B', 'X') == 0
@@ -11,3 +11,7 @@ assert userScoreRule('Z') == 3
 assert setToLoose('A') == 'Z'
 assert setToLoose('B') == 'X'
 assert setToLoose('C') == 'Y'
+
+assert setToWin('A') == 'Y'
+assert setToWin('B') == 'Z'
+assert setToWin('C') == 'X'
