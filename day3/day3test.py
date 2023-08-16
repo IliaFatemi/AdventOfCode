ccb71fe783368713018dadd751e710b2bd363efa
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-16 13:05:04 -0400

    Added unit tests for compareDepartments function

---
+++ day3/day3test.py
@@ -0,0 +1,9 @@
+from day3 import compareDepartments
+
+assert compareDepartments("vJrwpWtwJgWrhcsFMMfFFhFp") == ["p"]
+assert compareDepartments("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") == ["L"]
+assert compareDepartments("PmmdzqPrVvPwwTWBwg") == ["P"]
+assert compareDepartments("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn") == ["v"]
+assert compareDepartments("ttgJtRGJQctTZtZT") == ["t"]
+assert compareDepartments("CrZsJsPPZsGzwwsLwLmpwMDw") == ["s"]
+assert compareDepartments("gjqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSLG") == ["G", "L"]
