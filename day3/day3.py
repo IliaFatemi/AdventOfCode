735cea02da724dd09a2503f0b3995fe15298b4f4
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-16 13:04:37 -0400

    Implimented compareDepartments to check for duplicates and return them

--- day3/day3.py
+++ day3/day3.py
@@ -7,6 +7,20 @@ def getRuckSacks():
             numRuckSacks += 1
             rucksacks.append((numRuckSacks, i))
     return rucksacks
+
+def compareDepartments(sack):
+    duplicates = []
+    for i in range(int(len(sack)/2)):
+        for j in range(int(len(sack)/2)):
+            if sack[i] == sack[int(len(sack)/2)+j] and sack[i] not in duplicates:
+                duplicates.append(sack[i])
+    return duplicates
+
+# def findDuplicates():
+#     ruckSacks = getRuckSacks()
+#     for aSack in ruckSacks:
+#         for i in aSack[1]:
+#             pass
             
             
 if __name__ == "__main__":
