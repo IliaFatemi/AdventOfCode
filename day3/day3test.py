897f44b4e34fcc836f8794c2b214da5166371975
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-16 13:35:42 -0400

    Added unit tests for prioritizeItems

--- day3/day3test.py
+++ day3/day3test.py
@@ -1,4 +1,4 @@
-from day3 import compareDepartments
+from day3 import compareDepartments, prioritizeItems
 
 assert compareDepartments("vJrwpWtwJgWrhcsFMMfFFhFp") == ["p"]
 assert compareDepartments("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL") == ["L"]
@@ -7,3 +7,14 @@ assert compareDepartments("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn") == ["v"]
 assert compareDepartments("ttgJtRGJQctTZtZT") == ["t"]
 assert compareDepartments("CrZsJsPPZsGzwwsLwLmpwMDw") == ["s"]
 assert compareDepartments("gjqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSLG") == ["G", "L"]
+
+
+assert prioritizeItems(["p"]) == [("p", 16)]
+assert prioritizeItems(["L"]) == [("L", 38)]
+assert prioritizeItems(["p"]) == [("p", 16)]
+assert prioritizeItems(["v"]) == [("v", 22)]
+assert prioritizeItems(["t"]) == [("t", 20)]
+assert prioritizeItems(["s"]) == [("s", 19)]
+assert prioritizeItems(["G", "L"]) == [("G", 33), ("L", 38)]
+assert prioritizeItems(["Z"]) == [("Z", 52)]
+assert prioritizeItems(["z"]) == [("z", 26)]
