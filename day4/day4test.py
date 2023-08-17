7280052c33fa7ef58ed9972e6fdd8657a6ae2019
Ilia Fatemi <ilia_fatemi@sfu.ca>
2023-08-17 17:43:47 -0400

    Added unit tests for isOverlapping

--- day4/day4test.py
+++ day4/day4test.py
@@ -1,4 +1,4 @@
-from day4 import isInRange
+from day4 import isInRange, isOverlapping
 
 assert isInRange((1, ['2-4', '6-8'])) == False
 assert isInRange((2, ['2-3', '4-5'])) == False
@@ -6,3 +6,11 @@ assert isInRange((3, ['5-7', '7-9'])) == False
 assert isInRange((4, ['2-8', '3-7'])) == True
 assert isInRange((5, ['6-6', '4-6'])) == True
 assert isInRange((6, ['2-6', '4-8'])) == False
+
+
+assert isOverlapping((1, ['2-4', '6-8'])) == False
+assert isOverlapping((2, ['2-3', '4-5'])) == False
+assert isOverlapping((3, ['5-7', '7-9'])) == True
+assert isOverlapping((4, ['2-8', '3-7'])) == True
+assert isOverlapping((5, ['6-6', '4-6'])) == True
+assert isOverlapping((6, ['2-6', '4-8'])) == True
