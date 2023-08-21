064882d49a21d47544226f6b4f5c1946615df409
IliaFatemi <fatemiilia4@gmail.com>
2023-08-20 19:24:36 -0700

    Added unit tests for isDuplicate Function

--- day6/day6test.py
+++ day6/day6test.py
@@ -1,7 +1,12 @@
-from day6 import numCharProcessed
+from day6 import isDuplicate
 
-assert numCharProcessed('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7
-assert numCharProcessed('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
-assert numCharProcessed('nppdvjthqldpwncqszvftbrmjlhg') == 6
-assert numCharProcessed('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
-assert numCharProcessed('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') ==11
+assert isDuplicate('abcd') == False
+assert isDuplicate('abca') == True
+assert isDuplicate('abdd') == True
+assert isDuplicate('accd') == True
+
+# assert numCharProcessed('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7
+# assert numCharProcessed('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
+# assert numCharProcessed('nppdvjthqldpwncqszvftbrmjlhg') == 6
+# assert numCharProcessed('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
+# assert numCharProcessed('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') ==11
