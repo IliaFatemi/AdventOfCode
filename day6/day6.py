d772d6b24dc003a30589934fdb05da675a4c706c
IliaFatemi <fatemiilia4@gmail.com>
2023-08-20 19:57:28 -0700

    Completed part 2 of the puzzle

--- day6/day6.py
+++ day6/day6.py
@@ -22,7 +22,13 @@ def isDuplicate(characters):
 # Part 1
 def solve():
     with open('adventofcode.com_2022_day_6_input.txt', 'r') as file:
-        print('{} characters need to be processed before the first start-of-packet marker is detected'.format(findMarker(file.read(), 4)))
+        print('{} characters need to be processed before the first start-of-packet marker is detected'.format(findMarker(file.read())))
+
+# Part 2
+def solve2():
+    with open('adventofcode.com_2022_day_6_input.txt', 'r') as file:
+        print('{} characters need to be processed before the first start-of-packet marker is detected'.format(findMarker(file.read(), 14)))
 
 if __name__ == '__main__':
     solve()
+    solve2()
