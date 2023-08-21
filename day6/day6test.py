981c9f6df4173bbe8e50e0a5789532a60ad38539
IliaFatemi <fatemiilia4@gmail.com>
2023-08-20 19:53:21 -0700

    Added more unit tests to findMarker

--- day6/day6test.py
+++ day6/day6test.py
@@ -1,12 +1,18 @@
-from day6 import isDuplicate, findMarker
+from day6 import isDuplicate, findMarker, preProcessNum
 
 assert isDuplicate('abcd') == False
 assert isDuplicate('abca') == True
 assert isDuplicate('abdd') == True
 assert isDuplicate('accd') == True
 
-assert findMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7
-assert findMarker('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
-assert findMarker('nppdvjthqldpwncqszvftbrmjlhg') == 6
-assert findMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
-assert findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') ==11
+assert findMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 4) == 7
+assert findMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 4) == 5
+assert findMarker('nppdvjthqldpwncqszvftbrmjlhg', 4) == 6
+assert findMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 4) == 10
+assert findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 4) ==11
+
+assert findMarker('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 14) == 19
+assert findMarker('bvwbjplbgvbhsrlpgdmjqwftvncz', 14) == 23
+assert findMarker('nppdvjthqldpwncqszvftbrmjlhg', 14) == 23
+assert findMarker('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 14) == 29
+assert findMarker('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 14) == 26
