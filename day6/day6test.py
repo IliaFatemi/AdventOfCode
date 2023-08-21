4ee4deebc275869a6377d0ea63b9f56b199c746d
IliaFatemi <fatemiilia4@gmail.com>
2023-08-20 17:06:29 -0700

    Added unit tests for numCharProcessed

---
+++ day6/day6test.py
@@ -0,0 +1,7 @@
+from day6 import numCharProcessed
+
+assert numCharProcessed('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 7
+assert numCharProcessed('bvwbjplbgvbhsrlpgdmjqwftvncz') == 5
+assert numCharProcessed('nppdvjthqldpwncqszvftbrmjlhg') == 6
+assert numCharProcessed('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg') == 10
+assert numCharProcessed('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw') ==11
